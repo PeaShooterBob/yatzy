1039106a52bb8d140a9cc71e127af5200ee6f0e4
Robert Gu <robert.gu@ticketmaster.com>
2016-10-01 12:15:02 -0700

    Refactor chance method

--- yatzy.rb
+++ yatzy.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Yatzy
 
   def initialize(d1, d2, d3, d4, d5)
@@ -9,14 +10,8 @@ class Yatzy
     @dice[4] = d5
   end
 
-  def self.chance(d1, d2, d3, d4, d5)
-    total = 0
-    total += d1
-    total += d2
-    total += d3
-    total += d4
-    total += d5
-    return total
+  def self.chance(*dice)
+    dice.reduce(:+)
   end
 
   def self.yatzy(d1, d2, d3, d4, d5)
