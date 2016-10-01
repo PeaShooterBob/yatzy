cd6b932ff847e6f1fad22c652d5368731a0e76d4
Robert Gu <robert.gu@ticketmaster.com>
2016-10-01 13:21:47 -0700

    Scores using dice and category

--- test_yatzy.rb
+++ test_yatzy.rb
@@ -4,36 +4,18 @@ require 'minitest/autorun'
 require_relative 'yatzy'
 
 class YatzyTest < MiniTest::Unit::TestCase
-  def test_chance_scores_sum_of_all_dice
-    assert_equal 15, Yatzy.chance(2,3,4,5,1)
-    assert_equal 16, Yatzy.chance(3,3,4,5,1)
+  def test_score
+    assert_equal 50, Yatzy.score(4,4,4,4,4,"yatzy")
+    assert_equal 50, Yatzy.score(6,6,6,6,6,"yatzy")
+
+    assert_equal 15, Yatzy.score(2,3,4,5,1,"chance")
+    assert_equal 15, Yatzy.score(2,3,4,5,1,"chance")
+    assert_equal 16, Yatzy.score(3,3,4,5,1,"chance")
+
+    assert_equal 0, Yatzy.score(6,2,2,4,5,"one")
+    assert_equal 5, Yatzy.score(1,1,1,1,1,"one")
+
+    assert_equal 0, Yatzy.score(1,2,3,4,6,"five")
+    assert_equal 15, Yatzy.score(4,4,5,5,5,"five")
   end
-
-  def test_yatzy_scores_50
-    assert_equal 50, Yatzy.yatzy(4,4,4,4,4)
-    assert_equal 50, Yatzy.yatzy(6,6,6,6,6)
-    assert_equal 0, Yatzy.yatzy(6,6,6,6,3)
-  end
-
-  def test_score_dice_by_number
-    assert_equal 0, Yatzy.score_dice_by_number(6,2,2,4,5,"one")
-    assert_equal 1, Yatzy.score_dice_by_number(1,2,3,4,5,"one")
-    assert_equal 2, Yatzy.score_dice_by_number(1,2,1,4,5,"one")
-
-    assert_equal 0, Yatzy.score_dice_by_number(1,3,4,5,6,"two")
-    assert_equal 4, Yatzy.score_dice_by_number(1,2,3,2,6,"two")
-
-    assert_equal 0, Yatzy.score_dice_by_number(1,2,4,5,6,"three")
-    assert_equal 6, Yatzy.score_dice_by_number(1,2,3,2,3,"three")
-
-    assert_equal 0, Yatzy.score_dice_by_number(1,2,3,5,6,"four")
-    assert_equal 8, Yatzy.score_dice_by_number(4,4,5,5,5,"four")
-
-    assert_equal 0, Yatzy.score_dice_by_number(1,2,3,4,6,"five")
-    assert_equal 15, Yatzy.score_dice_by_number(4,4,5,5,5,"five")
-
-    assert_equal 0, Yatzy.score_dice_by_number(4,4,4,5,5,"six")
-    assert_equal 6, Yatzy.score_dice_by_number(4,4,6,5,5,"six")
-  end
-
 end
--- yatzy.rb
+++ yatzy.rb
@@ -1,26 +1,28 @@
-require 'pry'
 class Yatzy
+  DIE_NUMBER_VALUE = { "one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6 }
 
-  DIE_NUMBER_VALUE= {
-    "one" => 1,
-    "two" => 2,
-    "three" => 3,
-    "four" => 4,
-    "five" => 5,
-    "six" => 6
-  }
+  def self.score(*dice, category)
+    if category == "yatzy"
+      yatzy(dice)
+    elsif category == "chance"
+      chance(dice)
+    else
+      score_dice_by_number(dice, category)
+    end
+  end
 
-  def self.chance(*dice)
+  private
+
+  def self.chance(dice)
     dice.reduce(:+)
   end
 
-  def self.yatzy(*dice)
+  def self.yatzy(dice)
     dice.uniq.count == 1 ? 50 : 0
   end
 
-  def self.score_dice_by_number(*dice, die_number)
+  def self.score_dice_by_number(dice, die_number)
     die_value = DIE_NUMBER_VALUE[die_number]
     dice.select { |die| die == die_value}.count * die_value
   end
-
 end
