9b231426fa5c0463e3d60cb5c73b051f0a834bdd
Robert Gu <robert.gu@ticketmaster.com>
2016-10-01 12:42:47 -0700

    Can score dice given a certain die number

--- test_yatzy.rb
+++ test_yatzy.rb
@@ -15,40 +15,25 @@ class YatzyTest < MiniTest::Unit::TestCase
     assert_equal 0, Yatzy.yatzy(6,6,6,6,3)
   end
 
-  def test_ones
-    assert_equal 0, Yatzy.ones(6,2,2,4,5)
-    assert_equal 1, Yatzy.ones(1,2,3,4,5)
-    assert_equal 2, Yatzy.ones(1,2,1,4,5)
-    assert_equal 4, Yatzy.ones(1,2,1,1,1)
+  def test_score_dice_by_number
+    assert_equal 0, Yatzy.score_dice_by_number(6,2,2,4,5,"one")
+    assert_equal 1, Yatzy.score_dice_by_number(1,2,3,4,5,"one")
+    assert_equal 2, Yatzy.score_dice_by_number(1,2,1,4,5,"one")
+
+    assert_equal 0, Yatzy.score_dice_by_number(1,3,4,5,6,"two")
+    assert_equal 4, Yatzy.score_dice_by_number(1,2,3,2,6,"two")
+
+    assert_equal 0, Yatzy.score_dice_by_number(1,2,4,5,6,"three")
+    assert_equal 6, Yatzy.score_dice_by_number(1,2,3,2,3,"three")
+
+    assert_equal 0, Yatzy.score_dice_by_number(1,2,3,5,6,"four")
+    assert_equal 8, Yatzy.score_dice_by_number(4,4,5,5,5,"four")
+
+    assert_equal 0, Yatzy.score_dice_by_number(1,2,3,4,6,"five")
+    assert_equal 15, Yatzy.score_dice_by_number(4,4,5,5,5,"five")
+
+    assert_equal 0, Yatzy.score_dice_by_number(4,4,4,5,5,"six")
+    assert_equal 6, Yatzy.score_dice_by_number(4,4,6,5,5,"six")
   end
 
-  def test_twos
-    assert_equal 0, Yatzy.twos(1,3,4,5,6)
-    assert_equal 4, Yatzy.twos(1,2,3,2,6)
-    assert_equal 10, Yatzy.twos(2,2,2,2,2)
-  end
-
-  def test_threes
-    assert_equal 0, Yatzy.threes(1,2,4,5,6)
-    assert_equal 6, Yatzy.threes(1,2,3,2,3)
-    assert_equal 12, Yatzy.threes(2,3,3,3,3)
-  end
-
-  def test_fours
-    assert_equal 0, Yatzy.new(1,2,3,5,6).fours
-    assert_equal 4, Yatzy.new(4,5,5,5,5).fours
-    assert_equal 8, Yatzy.new(4,4,5,5,5).fours
-  end
-
-  def test_fives
-    assert_equal 0, Yatzy.new(1,2,3,4,6).fives
-    assert_equal 15, Yatzy.new(4,4,5,5,5).fives
-    assert_equal 20, Yatzy.new(4,5,5,5,5).fives
-  end
-
-  def test_sixes
-    assert_equal 0, Yatzy.new(4,4,4,5,5).sixes
-    assert_equal 6, Yatzy.new(4,4,6,5,5).sixes
-    assert_equal 18, Yatzy.new(6,5,6,6,5).sixes
-  end
 end
--- yatzy.rb
+++ yatzy.rb
@@ -1,14 +1,14 @@
 require 'pry'
 class Yatzy
 
-  def initialize(d1, d2, d3, d4, d5)
-    @dice = [0]*5
-    @dice[0] = d1
-    @dice[1] = d2
-    @dice[2] = d3
-    @dice[3] = d4
-    @dice[4] = d5
-  end
+  DIE_NUMBER_VALUE= {
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6
+  }
 
   def self.chance(*dice)
     dice.reduce(:+)
@@ -18,79 +18,9 @@ class Yatzy
     dice.uniq.count == 1 ? 50 : 0
   end
 
-  def self.ones(*dice)
-    dice.select { |die| die == 1 }.count
-  end
-
-  def self.twos( d1,  d2,  d3,  d4,  d5)
-    sum = 0
-    if (d1 == 2)
-      sum += 2
-    end
-    if (d2 == 2)
-      sum += 2
-    end
-    if (d3 == 2)
-      sum += 2
-    end
-    if (d4 == 2)
-      sum += 2
-    end
-    if (d5 == 2)
-      sum += 2
-    end
-    return sum
-  end
-
-  def self.threes( d1,  d2,  d3,  d4,  d5)
-    s = 0
-    if (d1 == 3)
-      s += 3
-    end
-    if (d2 == 3)
-      s += 3
-    end
-    if (d3 == 3)
-      s += 3
-    end
-    if (d4 == 3)
-      s += 3
-    end
-    if (d5 == 3)
-      s += 3
-    end
-    return s
-  end
-
-  def fours
-    sum = 0
-    for at in Array 0..4
-      if (@dice[at] == 4)
-        sum += 4
-      end
-    end
-    return sum
-  end
-
-  def fives()
-    s = 0
-    i = 0
-    for i in (Range.new(0, @dice.size))
-      if (@dice[i] == 5)
-        s = s + 5
-      end
-    end
-    s
-  end
-
-  def sixes
-    sum = 0
-    for at in 0..@dice.length
-      if (@dice[at] == 6)
-        sum = sum + 6
-      end
-    end
-    return sum
+  def self.score_dice_by_number(*dice, die_number)
+    die_value = DIE_NUMBER_VALUE[die_number]
+    dice.select { |die| die == die_value}.count * die_value
   end
 
 end
